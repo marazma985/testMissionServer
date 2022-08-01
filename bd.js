c57017a015ba2e4f34a85383c22eb4610d169623
maraz <marazma978@gmail.com>
2022-08-02 00:39:27 +0500

    commit with bd

--- bd.js
+++ bd.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize("testMission", "postgres", "Marmel985", {
+const sequelize = new Sequelize("d85bro562128pq", "dpcwwrizyisnii", "b8d980a351a7f51b0ff62f82e88c9d05cb8e48983d200dc84b97562f33ca29b8", {
   dialect: "postgres",
-  host:"localhost"
+  host:"ec2-18-214-35-70.compute-1.amazonaws.com"
 });
+
 
 module.exports = sequelize;
